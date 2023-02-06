80c67d2d18041976672fcef837a46df417a3ecc4
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2023-02-06 21:26:04 +0200

    06.02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,34 @@
-// var _ = require('lodash');
+var _ = require('lodash');
+
+const { times } = require("lodash");
 
 // import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
+player.on('play', function (timeupdate) {
+    
+// _.throttle(() => {
+//       localStorage.setItem(
+//         'videoplayer-current-time',
+//         JSON.stringify(timeupdate.seconds)
+//       );
+//     }, 1000);
 
-player.on('play', function () {
+
+
   console.log('played the video!');
+   localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(timeupdate.seconds)
+  );
 });
 
-player.getVideoTitle().then(function (title) {
-  console.log('BiggiSmoll:', title);
-});
+
+if (!localStorage.getItem('videoplayer-current-time')) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(0));
+  console.log('create Item');
+}
+let i = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(i);
+
