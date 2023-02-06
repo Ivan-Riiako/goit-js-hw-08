5acf0c909aa527187f103a9d2682fe4b6402a2ca
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2023-02-06 12:43:56 +0200

    06.02

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,34 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const refs = {
+  gallery: document.querySelector('div.gallery'),
+};
+
+function createPaletteItems() {
+  const items = galleryItems
+    .map(
+      ({
+        preview,
+        original,
+        description,
+      }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    )
+    .join('');
+  refs.gallery.insertAdjacentHTML('beforeend', items);
+}
+createPaletteItems();
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  //   captions: true,
+  //   captionSelector: "img",
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
