4dc944bacfb07d44152823b491c89f8e489ee45a
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2023-02-06 18:20:34 +0200

    06.02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,6 @@
 // var _ = require('lodash');
 
-import Player from '@vimeo/player';
+import Vimeo from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
@@ -10,5 +10,5 @@ player.on('play', function () {
 });
 
 player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
+  console.log('BiggiSmoll:', title);
 });
