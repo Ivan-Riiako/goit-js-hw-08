fe3b55b3c400662bf93d9b73c1b217abb566aab6
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2023-02-06 22:00:55 +0200

    06.02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,33 +2,30 @@ var _ = require('lodash');
 
 const { times } = require("lodash");
 
-// import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
-player.on('play', function (timeupdate) {
-    
-// _.throttle(() => {
-//       localStorage.setItem(
-//         'videoplayer-current-time',
-//         JSON.stringify(timeupdate.seconds)
-//       );
-//     }, 1000);
+document.addEventListener(
+  'click',
+  _.throttle(() => {
+    player.on('play', function (timeupdate) {
+      console.log('played the video!');
+      localStorage.setItem(
+        'videoplayer-current-time',
+        JSON.stringify(timeupdate.seconds)
+      );
+    });
+  }, 1000)
+);
 
+    if (!localStorage.getItem('videoplayer-current-time')) {
+      localStorage.setItem('videoplayer-current-time', JSON.stringify(0));
+      console.log('create Item');
+    }
+    let currentTime = JSON.parse(
+      localStorage.getItem('videoplayer-current-time')
+    );
+    player.setCurrentTime(currentTime);
 
-
-  console.log('played the video!');
-   localStorage.setItem(
-    'videoplayer-current-time',
-    JSON.stringify(timeupdate.seconds)
-  );
-});
-
-
-if (!localStorage.getItem('videoplayer-current-time')) {
-  localStorage.setItem('videoplayer-current-time', JSON.stringify(0));
-  console.log('create Item');
-}
-let i = JSON.parse(localStorage.getItem('videoplayer-current-time'));
-player.setCurrentTime(i);
-
+   
+ 
