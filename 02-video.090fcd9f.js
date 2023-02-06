57bb1f1f676ff810abafaa332cb80cf288234e1d
Ivan-Riiako <52213412+Ivan-Riiako@users.noreply.github.com>
2023-02-06 16:27:59 +0000

    Deploying to gh-pages from @ Ivan-Riiako/goit-js-hw-08@6933c81a93fc230d52e09fbfac5897749fbd70eb 🚀

---
+++ 02-video.090fcd9f.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("iframe"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("BiggiSmoll:",e)}));
+//# sourceMappingURL=02-video.090fcd9f.js.map
